0e2c4099176fb46a3050211052924d560f8191a6
abdo169 <44899237+abdo169@users.noreply.github.com>
2019-01-29 10:10:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -361,7 +361,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Chicago | Music 8 `)//حقوق دايموند كودز
+client.user.setGame(`Chicago | Music 9 `)//حقوق دايموند كودز
 client.user.setStatus("online")
 });
 
